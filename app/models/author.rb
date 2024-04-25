5dfc666e9f66db74677b1fbcf9ead205d735b549
fatmaelhot <fatmaelhot289@gmail.com>
2024-04-25 10:18:35 +0200

    lab_two_2

---
+++ app/models/author.rb
@@ -0,0 +1,5 @@
+class Author < ApplicationRecord
+    validates :name, :dob, :email, :phone_number, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+      
+end
